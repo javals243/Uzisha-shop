1c36b72068d8945262198a8c9dd1d04dc52d041f
valere chihisa <vchihisa@gmail.com>
2023-08-11 11:05:51 +0200

    minor updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-// export const server = "https://backend-ecommerce-22ic.onrender.com/api/v2";
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://backend-ecommerce-22ic.onrender.com/api/v2";
+// export const server = "http://localhost:8000/api/v2";
